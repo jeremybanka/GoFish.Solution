e5ed2c57f0349eac9d48f3d70afcdbb557f76876
Jeremy Banka <hello@jeremybanka.com>
2021-05-13 10:37:20 -0700

    ✨ add three models necessary for game
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

---
+++ GoFish/Models/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace GoFish.Models
+{
+  public class Player
+  {
+    public string Name { get; set; }
+    private static readonly List<Player> _allPlayers = new() { };
+    public List<Card> _hand = new() { };
+
+    public Player(string name)
+    {
+      Name = name;
+      _allPlayers.Add(this);
+    }
+  }
+
+  public class Card
+  {
+    public string Suit { get; set; }
+    public int Value { get; set; }
+    public int CardId { get; }
+    private List<Card> _instances = new() { };
+
+    public Card(string suit, int value)
+    {
+      Suit = suit;
+      Value = value;
+      _instances.Add(this);
+      CardId = _instances.Count;
+
+    }
+  }
+
+}
